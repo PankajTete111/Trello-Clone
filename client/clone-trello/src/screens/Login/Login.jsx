b82c2ea9f18327a12dba3301ae67afb7b6b54c50
Pankaj Tete <130996197+PankajTete111@users.noreply.github.com>
2024-02-20 12:23:30 +0530

    tostyfyand loader

--- client/clone-trello/src/screens/Login/Login.jsx
+++ client/clone-trello/src/screens/Login/Login.jsx
@@ -1,83 +1,376 @@
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useDataContext } from "../../context/State";
+// import Lodar from "../../components/Lodar/Lodar";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// const Login = () => {
+  
+//  const {setUserId,setUserName} = useDataContext();
+//  const[emailId,setEmail]=useState("");
+//  const[password,setPassword]=useState("");
+//  const navigate=useNavigate("");
+//  const [isLoading , setIsLoading] = useState(false);
+
+// const getData = async (e) => {
+//   e.preventDefault();
+//   setIsLoading(true);
+//   try {
+//     const response = await fetch('http://localhost:3050/api/v1/loginUser', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         email: emailId,
+//         password: password
+//       }),
+//     });
+
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+
+//     const responseData = await response.json();
+   
+//     console.log(responseData.user_id,"id");
+//     setUserId(responseData.user_id);
+//     setUserName(responseData.user_name)
+// console.log(responseData.user_name,"name")
+//     if (responseData.success) {
+//       setTimeout(() => {
+       
+//         navigate("/Home");
+//         setIsLoading(true);
+//       }, 5000);
+    
+//     } else {
+//       // Handle unsuccessful login
+//       // alert('Login failed. Please check your credentials.');
+   
+//     }
+//   } catch (error) {
+//     console.error('Error submitting data:', error);
+//     // alert('An error occurred. Please try again later.');
+//   }
+//   finally{
+//     setIsLoading(false);
+//   }
+// };
+
+//   return (
+//     <>
+//       <div className="" id="login-container">
+//         <div className="row pt-5 m-0">
+//           <div className="col-md-12">
+//             <div className="container d-flex justify-content-center" id="main1">
+//               <div className="card mt-4 " id="Card">
+//                 <div className="card-body text-center">
+//                   <h3>Login</h3>
+//                   <hr />
+//                   <form className="text-center" >
+//                     <div className="d-flex justify-content-center mb-4 mt-5">
+//                       <input
+//                         type="email"
+//                         placeholder="Enter Email"
+//                         className="form-control"
+//                         value={emailId}
+//                         onChange={(e)=>setEmail(e.target.value)}
+//                       />
+//                       {/* <div className="input-group-text " id="Email">
+//                         <i className="fas fa-envelope"></i>
+//                       </div> */}
+//                     </div>
+//                     <div className="d-flex justify-content-center mb-4 mt-2">
+//                       <input
+//                         type="password"
+//                         placeholder="Password"
+//                         className="form-control"
+//                         value={password}
+//                         onChange={(e)=>setPassword(e.target.value)}
+//                       />
+//                       {/* <div className="input-group-text" id="Password">
+//                         <i className="fas fa-lock"></i>
+//                       </div> */}
+//                     </div>
+//                     <div className="text-center">
+                      
+//                     </div>
+//                     <div className="row mt-3">
+//                       <div className="col-md-offset-10">
+//                         <button
+//                           className="btn btn-primary col-md-12"
+//                           type="submit"
+//                           onClick={getData}
+//                         >
+//                           Sign In
+//                         </button>
+//                       </div>
+//                       <div className="col-md-offset-10 mt-3">
+//                         <p>
+//                           Don't have an Account?
+//                         </p>
+//                       </div>
+//                     </div>
+//                   </form>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <ToastContainer className="tost-message" />
+//       {isLoading && <Lodar visible={isLoading}/>}
+//     </>
+//   );
+// };
+// export default Login;
+
+
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useDataContext } from "../../context/State";
+// import Lodar from "../../components/Lodar/Lodar";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// const Login = () => {
+//   const { setUserId, setUserName } = useDataContext();
+//   const [emailId, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate("");
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const loginUser = async (e) => {
+//     e.preventDefault();
+//     setIsLoading(true);
+//     try {
+//       const response = await fetch("http://localhost:3050/api/v1/loginUser", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           email: emailId,
+//           password: password,
+//         }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+
+//       const responseData = await response.json();
+
+//       console.log(responseData.user_id, "id");
+//       setUserId(responseData.user_id);
+//       setUserName(responseData.user_name);
+//       console.log(responseData.user_name, "name");
+
+//       if (responseData.success) {
+//         setTimeout(() => {
+//           navigate("/Home");
+//           setIsLoading(false);
+//         }, 5000);
+//       } else {
+//         // Handle unsuccessful login
+//         // alert('Login failed. Please check your credentials.');
+//         toast.error("Login failed. Please check your credentials.");
+//       }
+//     } catch (error) {
+//       console.error("Error submitting data:", error);
+//       toast.error("An error occurred. Please try again later.");
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//     //... Regex signup email validation
+//     const validationUserEmail = (email) => {
+//       const emailPattern =
+//         /^(?!\d+@)\w+([-+.']\w+)*@(?!\d+\.)\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+//       return emailPattern.test(email);
+//     };
+   
+    // const handlesubmit = () => {
+    //   if (emailId.trim() === "") {
+    //     toast.warn("please enter your email !");
+    //     // alert("please enter your email !");
+    //   } else if (!validationUserEmail(emailId)) {
+    //     toast.warn("Please enter a valid email !");
+    //     // alert("Please enter a valid email !");
+    //   } else if (password.trim() === "") {
+    //     toast.warn("Please enter a password !");
+    //     // alert("Please enter a password !");
+    //   } else {
+    //     loginUser();
+    //     setEmail("");
+    //     setPassword("");
+    //     navigate("/Home");
+    //   }
+//     };
+
+//   return (
+//     <>
+//       <div className="" id="login-container">
+//         <div className="row pt-5 m-0">
+//           <div className="col-md-12">
+//             <div
+//               className="container d-flex justify-content-center"
+//               id="main1"
+//             >
+//               <div className="card mt-4 " id="Card">
+//                 <div className="card-body text-center">
+//                   <h3>Login</h3>
+//                   <hr />
+//                   <form className="text-center">
+//                     <div className="d-flex justify-content-center mb-4 mt-5">
+//                       <input
+//                         type="email"
+//                         placeholder="Enter Email"
+//                         className="form-control"
+//                         value={emailId}
+//                         onChange={(e) => setEmail(e.target.value)}
+//                       />
+//                     </div>
+//                     <div className="d-flex justify-content-center mb-4 mt-2">
+//                       <input
+//                         type="password"
+//                         placeholder="Password"
+//                         className="form-control"
+//                         value={password}
+//                         onChange={(e) => setPassword(e.target.value)}
+//                       />
+//                     </div>
+//                     <div className="text-center"></div>
+//                     <div className="row mt-3">
+//                       <div className="col-md-offset-10">
+//                         <button
+//                           className="btn btn-primary col-md-12"
+//                           type="submit"
+//                           onClick={getData}
+//                         >
+//                           Sign In
+//                         </button>
+//                       </div>
+//                       <div className="col-md-offset-10 mt-3">
+//                         <p>Don't have an Account?</p>
+//                       </div>
+//                     </div>
+//                   </form>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <ToastContainer className="toast-message" />
+//       {isLoading && <Lodar visible={isLoading} />}
+//     </>
+//   );
+// };
+
+// export default Login;
+
+
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDataContext } from "../../context/State";
 import Lodar from "../../components/Lodar/Lodar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Login = () => {
-  
- const {setUserId,setUserName} = useDataContext();
- const[emailId,setEmail]=useState("");
- const[password,setPassword]=useState("");
- const navigate=useNavigate("");
- const [isLoading , setIsLoading] = useState(false);
+  const { setUserId, setUserName } = useDataContext();
+  const [emailId, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-const getData = async (e) => {
-  e.preventDefault();
-  setIsLoading(true);
-  try {
-    const response = await fetch('http://localhost:3050/api/v1/loginUser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: emailId,
-        password: password
-      }),
-    });
+  const loginUser = async () => {
+    try {
+      const response = await fetch("http://localhost:3050/api/v1/loginUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: emailId,
+          password: password,
+        }),
+      });
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+
+      console.log(responseData.user_id, "id");
+      setUserId(responseData.user_id);
+      setUserName(responseData.user_name);
+      console.log(responseData.user_name, "name");
+
+      if (!responseData.success) {
+        toast.error("Login failed. Please check your credentials.");
+      }
+    } catch (error) {
+      console.error("Error submitting data:", error);
+      toast.error("An error occurred. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    const responseData = await response.json();
-   
-    console.log(responseData.user_id,"id");
-    setUserId(responseData.user_id);
-    setUserName(responseData.user_name)
-console.log(responseData.user_name,"name")
-    if (responseData.success) {
-      setTimeout(() => {
-       
-        navigate("/Home");
-        setIsLoading(true);
-      }, 5000);
-    
+  // Regex signup email validation
+  const validationUserEmail = (email) => {
+    const emailPattern =
+      /^(?!\d+@)\w+([-+.']\w+)*@(?!\d+\.)\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    return emailPattern.test(email);
+  };
+
+  const handlesubmit = () => {
+    if (emailId.trim() === "") {
+      toast.warn("please enter your email !");
+      // alert("please enter your email !");
+    } else if (!validationUserEmail(emailId)) {
+      toast.warn("Please enter a valid email !");
+      // alert("Please enter a valid email !");
+    } else if (password.trim() === "") {
+      toast.warn("Please enter a password !");
+      // alert("Please enter a password !");
     } else {
-      // Handle unsuccessful login
-      // alert('Login failed. Please check your credentials.');
-   
+      loginUser();
+      setEmail("");
+      setPassword("");
+      navigate("/Home");
     }
-  } catch (error) {
-    console.error('Error submitting data:', error);
-    // alert('An error occurred. Please try again later.');
-  }
-  finally{
-    setIsLoading(false);
-  }
-};
+  };
 
   return (
     <>
       <div className="" id="login-container">
         <div className="row pt-5 m-0">
           <div className="col-md-12">
-            <div className="container d-flex justify-content-center" id="main1">
+            <div
+              className="container d-flex justify-content-center"
+              id="main1"
+            >
               <div className="card mt-4 " id="Card">
                 <div className="card-body text-center">
                   <h3>Login</h3>
                   <hr />
-                  <form className="text-center" >
+                  <form className="text-center">
                     <div className="d-flex justify-content-center mb-4 mt-5">
                       <input
                         type="email"
                         placeholder="Enter Email"
                         className="form-control"
                         value={emailId}
-                        onChange={(e)=>setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                       />
-                      {/* <div className="input-group-text " id="Email">
-                        <i className="fas fa-envelope"></i>
-                      </div> */}
                     </div>
                     <div className="d-flex justify-content-center mb-4 mt-2">
                       <input
@@ -85,29 +378,21 @@ console.log(responseData.user_name,"name")
                         placeholder="Password"
                         className="form-control"
                         value={password}
-                        onChange={(e)=>setPassword(e.target.value)}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
-                      {/* <div className="input-group-text" id="Password">
-                        <i className="fas fa-lock"></i>
-                      </div> */}
-                    </div>
-                    <div className="text-center">
-                      
                     </div>
                     <div className="row mt-3">
                       <div className="col-md-offset-10">
                         <button
                           className="btn btn-primary col-md-12"
                           type="submit"
-                          onClick={getData}
+                          onClick={handlesubmit}
                         >
                           Sign In
                         </button>
                       </div>
                       <div className="col-md-offset-10 mt-3">
-                        <p>
-                          Don't have an Account?
-                        </p>
+                        <p>Don't have an Account?</p>
                       </div>
                     </div>
                   </form>
@@ -117,8 +402,11 @@ console.log(responseData.user_name,"name")
           </div>
         </div>
       </div>
-      {isLoading && <Lodar visible={isLoading}/>}
+      <ToastContainer className="toast-message" />
+      {isLoading && <Lodar visible={isLoading} />}
     </>
   );
 };
+
 export default Login;
+
--- client/clone-trello/src/screens/SignUp/Signup.jsx
+++ client/clone-trello/src/screens/SignUp/Signup.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import "../SignUp/Signup.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Lodar from "../../components/Lodar/Lodar";
+
 
 const Signup = () => {
    const[full_name,setFullName]=useState("");
    const[emailId,setEmail]=useState("");
    const[password,setPassword]=useState("");
    const[contact_no,setContact]=useState("");
+   const [isLoading, setIsLoading] = useState(false);
+
    const navigate=useNavigate("");
-  
+   
    const setdata = async () => {
     try {
       const response = await fetch('http://localhost:3050/api/v1/createUser', {
@@ -30,21 +36,55 @@ const Signup = () => {
   
       const responseData = await response.json();
       console.log('Response from server:', responseData);
-      navigate("/Login");
+      setTimeout(() => {
+        navigate("/Login");
+        setIsLoading(true);
+      }, 2000);
+      // navigate("/Login");
     } catch (error) {
       console.error('Error during fetch:', error);
     }
+
   };
   
+    //... Regex signup email validation
+    const validationUserEmail = (email) => {
+      const emailPattern =
+        /^(?!\d+@)\w+([-+.']\w+)*@(?!\d+\.)\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      return emailPattern.test(email);
+    };
+
+  const handlesubmit = () =>{
+    const mobileReg = /^[0-9]+$/;
+    if(full_name.trim() === ''){
+      toast.warn('please enter your name !');
+    }else if(emailId.trim() === ''){
+      toast.warn('please enter your email !');
+    }else if(!validationUserEmail(emailId)){
+      toast.warn("Please enter a valid email !");
+    }else if(password.trim() === ''){
+      toast.warn("Please enter a password !");
+    }else if(contact_no.trim() === ''){
+      toast.warn("Please enter phone number !");
+    }else if(!mobileReg.test(contact_no)){
+      toast.warn("Please enter valid phone number !");
+    }else{
+      setdata();
+      setFullName('');
+      setEmail('');
+      setPassword('');
+      setContact('')
+    }
+  }
 
   return (
     <section className="signup-section">
       <div className="text-center">
       </div>
-      <div className="container mt-3">
+      <div className="container pt-5">
         <div className="row justify-content-center">
           <div className="col-md-6" id="card-main">
-            <div className="card mt-0">
+            <div className="signupucard card mt-0">
               <div className="card-header text-center register" id="register">
                 <h3 className="h3_registration">Registration Form</h3>
               </div>
@@ -97,6 +137,7 @@ const Signup = () => {
                       Phone Number <span className="star">*</span>
                     </label>
                     <input
+                      maxLength={10}
                       type="number"
                       className="form-control"
                       placeholder="Phone Number"
@@ -106,7 +147,8 @@ const Signup = () => {
                   </div>
                   <button
                     className="btn btn-primary col-12"
-                    onClick={setdata}
+                    // onClick={setdata}
+                    onClick={handlesubmit}
                   >
                     SIGN UP
                   </button>
@@ -116,8 +158,11 @@ const Signup = () => {
           </div>
         </div>
       </div>
+      <ToastContainer className="tost-message" />
+      {isLoading && <Lodar visible={isLoading} />}
     </section>
   );
 };
 
 export default Signup;
+
