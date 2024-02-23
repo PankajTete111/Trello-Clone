c6cdb0beb32621e049d0bd694f0f814063041ca1
Pankaj Tete <130996197+PankajTete111@users.noreply.github.com>
2024-02-23 17:49:58 +0530

    controller

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const UserController = require('../controller/UserController');
-// const LoginController = require('../controller/LoginController')
-
-// const router = express.Router();
-
-// // Define your routes here
-// router.post('/loginUser', LoginController.loginUser);
-// router.post('/createUser', UserController.insertUser);
-// router.post('/createBoard', UserController.insertBoard);
-// router.get('/boardDetails/:usiKey',UserController.getBoardDetails)
-// router.post('/createCard', UserController.createCard);
-// router.get('/cardDetails/:usiKey', UserController.getCardDetails);
-// // router.post('/verifyToken', LoginController.verifyToken);
-
-
-// module.exports = router;
-
 const express = require('express');
 const UserController = require('../controller/UserController');
 const LoginController = require('../controller/LoginController');
